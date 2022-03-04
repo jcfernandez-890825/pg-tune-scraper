c131a9054dcf7ff9a6ae9ed2300afdce28230364
Nicolas Julian <nicolasjulian21@gmail.com>
2022-03-04 14:09:19 +0700

    - Fix, update automation.sh

--- automation.sh
+++ automation.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 WORK_DIR=/tmp/pg-tune-env
+if [[ $RESULT == '' ]]; then
+  RESULT=~/pg-tune.conf
+else
+  RESULT=$1
+fi
 
 ## Get system information
 os_type=$(os=`uname`;echo "${os,,}")
@@ -10,22 +15,21 @@ storage=ssd
 db_type=oltp
 
 ## Install packages
-sudo apt install chromium-browser python3.8-venv -y && sudo snap install chromium
-
+#sudo apt install chromium-browser python3.8-venv -y && sudo snap install chromium
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -qq chromium-browser python3.8-venv -y < /dev/null > /dev/null && sudo snap install chromium
 ## Setup tools
 python3 -m venv $WORK_DIR
 source $WORK_DIR/bin/activate
-export PWD=$WORK_DIR
-git clone https://github.com/nicolasjulian/pg-tune-scraper.git
-cd pg-tune-scraper
-pip3 install -r requirements.txt
-sed -i 's|./chromedriver|/snap/bin/chromium.chromedriver|g' scrape.py
+cd $WORK_DIR && git clone https://github.com/nicolasjulian/pg-tune-scraper.git
+cd $WORK_DIR/pg-tune-scraper && pip3 install -r requirements.txt
+cd $WORK_DIR/pg-tune-scraper && sed -i 's|./chromedriver|/snap/bin/chromium.chromedriver|g' scrape.py
 
 ## Get config
-./scrape.py -d $posgre_ver -o $os_type -dt $db_type -r $ram_size -c $cpu -st $storage > ~/pg-tune.conf
+cd $WORK_DIR/pg-tune-scraper && ./scrape.py -d $posgre_ver -o $os_type -dt $db_type -r $ram_size -c $cpu -st $storage | tee $RESULT
 
 ## Clean up workspace & chromium
-sudo apt remove chromium-browser --purge -y
+#sudo apt remove chromium-browser --purge -y -qq > /dev/null
+sudo DEBIAN_FRONTEND=noninteractive apt-get remove --purge -y -qq chromium-browser < /dev/null > /dev/null
 sudo snap remove chromium
 export PWD=~/
 rm -rf $WORK_DIR
