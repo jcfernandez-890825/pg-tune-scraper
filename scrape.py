e5e0ff6a3d6c0b6eb559dbc0cfa8c89533e3a004
Nicolas Julian <nicolasjulian21@gmail.com>
2022-03-04 14:34:26 +0700

    Fix Automation.sh, adding no-sanbox when using root user.

--- scrape.py
+++ scrape.py
@@ -25,6 +25,8 @@ args = parser.parse_args()
 
 capabilities = dict(webdriver.DesiredCapabilities.CHROME)
 options = webdriver.ChromeOptions()
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-dev-shm-usage')
 options.add_argument('--ignore-ssl-errors=yes')
 options.add_argument('--ignore-certificate-errors')
 options.add_argument("headless")
